c60f43b4f078518612eda316c2e4e64996e94e3d
AniketIXR <aniketroy2003@gmail.com>
2023-05-21 23:42:05 +0530

    timetable controller for crud operation ,and route added

--- controllers/timetableController.js
+++ controllers/timetableController.js
@@ -2,7 +2,7 @@ const Timetable = require('../models/timetableModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.getAllTimetables = catchAsync(async (req, res, next) => {
-  const timetables = await Timetable.find().populate('course').populate('prof');
+  const timetables = await Timetable.find().populate('timetables').populate('prof');
   res.status(200).json({
     status: 'success',
     results: timetables.length,
@@ -11,3 +11,53 @@ exports.getAllTimetables = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+exports.getTimetable =catchAsync(async(req, res,next)=>{
+  const timetables =await Timetable.findById(req.params.id); 
+  res.status(200).json({
+      status:'success',
+      data : {
+          timetables
+      }
+  });
+});
+
+exports.createTimetable =catchAsync( async (req, res,next)=>{
+
+  const newTimetable = await Timetable.create(req.body);
+  res.status(201).json({
+      status:"success",
+      data: {
+          timetable : newTimetable
+      }
+  });   
+
+});
+
+exports.updateTimetable= catchAsync(async (req, res,next) => {
+
+const timetables = await Timetable.findByIdAndUpdate(req.params.id, req.body, {
+  new: true,
+  runValidators : true,
+});
+res.status(200).json({
+  status: "success",
+  data : {
+      timetables
+  } 
+});
+
+});
+
+exports.deleteTimetable=catchAsync(async (req,res,next)=>{
+
+await Timetable.findByIdAndDelete(req.params.id);
+res.status(204).json({
+  status: "success",
+  data:null
+});
+
+});
+
+
+
--- routes/timetableRoutes.js
+++ routes/timetableRoutes.js
@@ -4,8 +4,12 @@ const timetableController = require('../controllers/timetableController');
 
 const router = express.Router();
 
-router
-  .route('/')
-  .get(timetableController.getAllTimetables);
+  router.route('/')
+     .get(timetableController.getAllTimetables)
+     .post(timetableController.createTimetable);
+router.route('/:id')
+      .get(timetableController.getTimetable)
+      .patch(timetableController.updateTimetable)
+      .delete(timetableController.deleteTimetable);
 
 module.exports = router;
